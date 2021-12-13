6693c7813323ce74cd15905637d87523866010b8
Camilo Romero <camiloaromero23@gmail.com>
2021-12-13 00:40:07 -0500

    Include SortStringByCharacter function

--- utils/strings.go
+++ utils/strings.go
@@ -1,9 +1,20 @@
 package utils
 
-import "regexp"
+import (
+	"regexp"
+	"sort"
+)
 
 func RemoveDuplicateWhitespace(s string) string {
 	spaceRegex := regexp.MustCompile(`\s+`)
 	clean := spaceRegex.ReplaceAllString(s, " ")
 	return clean
+}
+
+func SortStringByCharacter(s string) string {
+	r := []rune(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
 }
